4b97a3e18222da35080f4059eaaad45f1829b346
Bobby <jubale1328@zoho.com>
2023-11-12 11:06:08 -0800

    unauthorized deleting chirp

---
+++ app/Policies/ChirpPolicy.php
@@ -0,0 +1,49 @@
+<?php
+ 
+ use Illuminate\Auth\Access\HandlesAuthorization;
+ 
+ class ChirpPolicy
+{
+    use HandlesAuthorization;
+ 
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+ 
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Chirp $chirp): bool
+    {
+        //
+    }
+ 
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+ 
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Chirp $chirp): bool
+    {
+        return $chirp->user()->is($user);
+    }
+ 
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Chirp $chirp): bool
+    {
+        //
+        return $this->update($user, $chirp);
+    }
+}
